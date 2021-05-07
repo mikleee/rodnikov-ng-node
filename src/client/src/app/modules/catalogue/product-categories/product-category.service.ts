8aff657565d2e2d530abc13f7c29787367e78e02
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2021-05-07 14:46:55 +0300

    observable calls

--- src/client/src/app/modules/catalogue/product-categories/product-category.service.ts
+++ src/client/src/app/modules/catalogue/product-categories/product-category.service.ts
@@ -1,31 +1,25 @@
 import {Injectable} from '@angular/core';
-import {Observable, ReplaySubject, Subject} from "rxjs";
 import {HttpService} from "../../shared/service/http.service";
 import {ProductCategory, ProductSupplier} from "../catalogue.models";
 import {first, map} from "rxjs/operators";
 import {modelsToMap} from "../../shared/model/base.model";
+import {Observable} from "rxjs";
 
 
 @Injectable({providedIn: "root"})
 export class ProductCategoryService {
-  private categories$ = new ReplaySubject<{ [key: string]: ProductCategory }>();
-  private needLoad = true;
 
   constructor(private http: HttpService) {
 
   }
 
   getProductCategories(): Observable<{ [key: string]: ProductCategory }> {
-    if (this.needLoad) {
-      this.loadProductCategories();
-    }
-    return this.categories$;
-  }
-
-  getProductCategoriesTree(): Observable<ProductCategory[]> {
-    return this.categories$.pipe(
-      map(value => this.buildTree(Object.values(value)))
-    );
+    return this.http.get('/api/categories/list')
+      .pipe(
+        first(),
+        map(result => result || []),
+        map(result => modelsToMap(result as ProductCategory[])),
+      )
   }
 
   getProductCategory(id: String): Promise<ProductCategory> {
@@ -33,15 +27,11 @@ export class ProductCategoryService {
   }
 
   submitProductCategory(supplier: ProductCategory): Promise<ProductCategory> {
-    let promise = this.http.post('/api/categories/category/submit', supplier).toPromise();
-    promise.then(value => this.loadProductCategories());
-    return promise
+    return this.http.post('/api/categories/category/submit', supplier).toPromise();
   }
 
   deleteGroup(id: String): Promise<ProductSupplier> {
-    let promise = this.http.post('/api/categories/category/delete/' + id).toPromise();
-    promise.then(value => this.loadProductCategories());
-    return promise
+    return this.http.post('/api/categories/category/delete/' + id).toPromise();
   }
 
   assignSubcategories(input: ProductCategory[]): ProductCategory[] {
@@ -69,23 +59,6 @@ export class ProductCategoryService {
 
   buildTree(input: ProductCategory[]): ProductCategory[] {
     return this.assignSubcategories(input).filter(g => !g.parent);
-  }
-
-  loadProductCategories(): Subject<{ [key: string]: ProductCategory }> {
-    this.http.get('/api/categories/list')
-      .pipe(
-        first(),
-        map(result => result || []),
-        map(result => modelsToMap(result as ProductCategory[])),
-      )
-      .subscribe(
-        value => {
-          this.needLoad = false;
-          this.categories$.next(value)
-        },
-        value => this.categories$.error(value.message),
-      )
-    return this.categories$;
   }
 
 
--- src/client/src/app/modules/catalogue/product-suppliers/product-suppliers.service.ts
+++ src/client/src/app/modules/catalogue/product-suppliers/product-suppliers.service.ts
@@ -1,25 +1,25 @@
 import {Injectable} from '@angular/core';
-import {Observable, ReplaySubject} from "rxjs";
 import {HttpService} from "../../shared/service/http.service";
 import {ProductSupplier} from "../catalogue.models";
 import {first, map} from "rxjs/operators";
 import {modelsToMap} from "../../shared/model/base.model";
+import {Observable} from "rxjs";
 
 
 @Injectable({providedIn: "root"})
 export class ProductSuppliersService {
-  private suppliers$ = new ReplaySubject<{ [key: string]: ProductSupplier }>();
-  private needLoad = true;
 
   constructor(private http: HttpService) {
 
   }
 
   getSuppliers(): Observable<{ [key: string]: ProductSupplier }> {
-    if (this.needLoad) {
-      this.loadSuppliers();
-    }
-    return this.suppliers$
+    return this.http.get('/api/suppliers/list')
+      .pipe(
+        first(),
+        map(result => result || []),
+        map(result => modelsToMap(result as ProductSupplier[])),
+      );
   }
 
   getSupplier(id: String): Promise<ProductSupplier> {
@@ -31,36 +31,15 @@ export class ProductSuppliersService {
   }
 
   submitSupplier(supplier: FormData): Promise<ProductSupplier> {
-    let promise = this.http.postMultipartFormData('/api/suppliers/supplier/submit', supplier);
-    promise.then(() => this.loadSuppliers());
-    return promise
+    return this.http.postMultipartFormData('/api/suppliers/supplier/submit', supplier)
   }
 
   deleteSupplier(id: String): Promise<ProductSupplier> {
-    let promise = this.http.post('/api/suppliers/supplier/delete/' + id)
+    return this.http.post('/api/suppliers/supplier/delete/' + id)
       .pipe(
         first()
       )
       .toPromise();
-    promise.then(() => this.loadSuppliers());
-    return promise
-  }
-
-
-  private loadSuppliers() {
-    this.http.get('/api/suppliers/list')
-      .pipe(
-        first(),
-        map(result => result || []),
-        map(result => modelsToMap(result as ProductSupplier[])),
-      )
-      .subscribe(
-        value => {
-          this.needLoad = false;
-          this.suppliers$.next(value)
-        },
-        value => this.suppliers$.error(value.message),
-      )
   }
 
 
