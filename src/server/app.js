f36d7ba45843c9ce862247f42261e3acbf642b7b
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2021-05-11 23:22:26 +0300

    deploy

--- src/server/app.js
+++ src/server/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const fileUpload = require('express-fileupload');
-const indexRouter = require('./routes/index');
 const db = require('./db/db');
 
 const app = express();
@@ -13,17 +12,18 @@ app.use(express.urlencoded());
 app.use(fileUpload({createParentPath: true, debug: true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/ngjs')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public/view'));
 app.set('view engine', 'ejs');
 
-app.use('/', indexRouter);
 app.use('/api/suppliers', require('./routes/product.suppliers.routes'));
 app.use('/api/categories', require('./routes/product.categories.routes'));
 app.use('/api/products', require('./routes/products.routes'));
 app.use('/api/document', require('./routes/document.routes'));
+app.use('/', require('./routes/render'));
 
 
 module.exports = app;
